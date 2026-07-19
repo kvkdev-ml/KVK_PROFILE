9d028c24c567457aecd2763503541ebe56f8e66e
kvkdev-ml <kvkwork05@gmail.com>
2026-07-19 11:55:21 +0530

    app fix

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template,request,jsonify
+from flask_cors import CORS
 import pickle as pk
 app = Flask(__name__)
+CORS(app=app)
 route_dict = {
     "/": "portfolio.html",
     "/KVK": "portfolio.html",
